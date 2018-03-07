478d59a5254a8d212cd089edbb1b5cb300cac2ee
SongChujun <30452818+SongChujun@users.noreply.github.com>
2018-03-07 10:49:03 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,43 +1,12 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#define MAX_SIZE 1024
- 
-int main(void)
+#include<stdio.h>
+#include<linux/kernel.h>
+#include<sys/syscall.h>
+#include<unistd.h>
+int main()
 {
-	int fd;
-	char buf[MAX_SIZE];	//缓冲区
-	char get[MAX_SIZE];	//要写入的信息
- 
-	char dir[50] = "/dev/mydevice";
- 
-	fd = open(dir, O_RDWR | O_NONBLOCK);
- 
-	if (fd != -1)
-	{
-		//读初始信息
-		// read(fd, buf, sizeof(buf));
-		// printf("%s\n", buf);
- 
-		//写信息
-		printf("input :");
-		gets(get);
-		write(fd, get, sizeof(get));
- 
-		//读刚才写的信息
-		read(fd, buf, sizeof(buf)); 
-		printf("output: %s\n", buf);
-		
-		close(fd);
-		return 0;
-	}
-	else
-	{
-		printf("Device open failed\n");
-		return -1;
-	}
+    long int res=syscall(358);
+    long int res2=syscall(359,"/home/songchujun/Desktop/syscall/copy.c","/home/songchujun/Desktop/syscall/copyy.c");
+    printf("res=%ld\n",res);
+    printf("res=%ld\n",res2);
+    return 0;
 }
